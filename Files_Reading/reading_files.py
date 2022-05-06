5fa8446c0dc69e5b7c517483ed2f4357851943fe
SalesBandeira <salesbandeira20@gmail.com>
2022-05-06 18:58:42 -0300

    Studies with open()

--- Files_Reading/reading_files.py
+++ Files_Reading/reading_files.py
@@ -1,3 +1,22 @@
+"""
+A função open() é utilizada para acessar arquivos.
+
+a função aceita dois parametros, a primeira é o nome ou path para o arquivo. 
+A segunda é o que iremos fazer com o arquivo segundo o sequinte modelo, sempre entre aspas:
+
+'r' abre par leitura somente, se nada for informado, essa opção é default.
+'w'	abre para leitura, apaga todos os dados antes.
+'x'	ccria um novo arquivo e o abre para leitura,
+'a'	abre para leitura, adcionando ao final da ultima linha
+'b'	binary mode
+'t'	text mode (default)
+'+'	abre para atualizar, leitura e escrita
+'U'	universal newline mode (deprecated)
+
+"""
+
+# Chamamos os atributos .read ou .write par usarmos o arquivo de fato.
+
 class File:
     def write_file(file):
         with open(file,'a+') as arquivo:
